2077e99a14875fa1abedf55c643696e5da22e198
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-03-28 18:13:40 +0300

    Take piece position into account in eval function

--- src/ai/main.ts
+++ src/ai/main.ts
@@ -1,24 +1,34 @@
 import { Board, Color, Move, OccupiedSquare, Piece } from '../types.ts'
 import { chess } from '../util/chess.ts'
+import { piecePositions, pieceValues } from './tables.ts'
 import minimax from './minimax.ts'
-
-const pieceValues = {
-  p: 1,
-  n: 3,
-  b: 3,
-  r: 5,
-  q: 9,
-  k: 100,
-}
 
 const getOccupiedSquares = (board: Board): OccupiedSquare[] =>
   board.flat().filter(Boolean) as OccupiedSquare[]
 
 const getPieces = (pieceColor: string, squares: OccupiedSquare[]) =>
-  squares.filter(({ color }) => color === pieceColor).map(({ type }) => type)
+  squares.filter(({ color }) => color === pieceColor)
 
-const calculateValue = (pieces: Piece[]) => {
-  const values = pieces.map((piece) => pieceValues[piece])
+const getPieceValue = (
+  color: Color,
+  { type, square }: OccupiedSquare,
+): number => {
+  const pieceValue = pieceValues[type as Piece]
+
+  const positions = color === 'w'
+    ? piecePositions[type as Piece].map((row) => row.reverse()).reverse()
+    : piecePositions[type as Piece]
+
+  const [file, rank] = square.split('') as [string, string]
+  const positionValue = positions[Number(rank) - 1][file.charCodeAt(0) - 97]
+
+  const value = pieceValue + positionValue
+
+  return value
+}
+
+const calculateValue = (color: Color, pieces: OccupiedSquare[]) => {
+  const values = pieces.map((piece) => getPieceValue(color, piece))
 
   return values.reduce((acc: number, val: number) => acc + val, 0)
 }
@@ -29,8 +39,8 @@ export const evaluateBoard = (board: Board, color: Color) => {
   const whitePieces = getPieces('w', squares)
   const blackPieces = getPieces('b', squares)
 
-  const whiteValue = calculateValue(whitePieces)
-  const blackValue = calculateValue(blackPieces)
+  const whiteValue = calculateValue('w', whitePieces)
+  const blackValue = calculateValue('b', blackPieces)
 
   const score = color === 'w'
     ? whiteValue - blackValue
