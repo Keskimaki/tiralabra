567c1bddc363c5b97c981149bf96b0f7c1636736
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-01 10:16:31 +0300

    Error handling for missing lichess token

--- src/util/config.ts
+++ src/util/config.ts
@@ -3,3 +3,5 @@ import { load } from 'std/dotenv/mod.ts'
 const env = await load()
 
 export const LICHESS_BOT_TOKEN = env.LICHESS_BOT_TOKEN
+
+if (!LICHESS_BOT_TOKEN) throw new Error('LICHESS_BOT_TOKEN not found in .env file')
