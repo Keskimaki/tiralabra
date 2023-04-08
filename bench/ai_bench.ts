1bca8aaf23f0e47489811509857a135c3e350d17
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-08 17:48:36 +0300

    Update benchmarks

--- bench/ai_bench.ts
+++ bench/ai_bench.ts
@@ -1,14 +1,16 @@
-import { chess } from '../src/util/chess.ts'
 import { evaluateBoard } from '../src/ai/main.ts'
 import minimax from '../src/ai/minimax.ts'
+import { initializeGame } from '../src/chess/main.ts'
 
 Deno.bench('Eval function', () => {
-  const board = chess.board()
+  const { board } = initializeGame('test')
   evaluateBoard(board, 'w')
 })
 
 for (let i = 1; i < 5; i++) {
+  const game = initializeGame('test')
+
   Deno.bench(`Minimax ${i} deep`, () => {
-    minimax(chess, i, 'w')
+    minimax(game, i, 'w')
   })
 }
